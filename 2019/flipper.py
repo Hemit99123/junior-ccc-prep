0e654f5dfd2d3550bce32a21473b8f9786d12b2c
Hemit Patel <hemitvpatel@gmail.com>
2025-01-15 10:34:30 -0500

    added a function

--- 2019/flipper.py
+++ 2019/flipper.py
@@ -6,23 +6,32 @@
 # 15 jan 2025
 
 instructions = input()
+
 top = ["1","2"]
 bottom = ["3","4"]
 
+def flip_horizonal(top, bottom):
+    new_bottom = top
+    new_top = bottom
+
+    return new_bottom, new_top
+
+def flip_vertical(top, bottom):
+    new_bottom = [bottom[1], bottom[0]]
+    new_top = [top[1], top[0]]
+
+    return new_bottom, new_top
+
 for instruction in instructions:
     if (instruction == "H"):
-        new_bottom = [top[0], top[1]]
-        new_top = [bottom[0], bottom[1]]
-
-        top = new_top
-        bottom = new_bottom
+        result = flip_horizonal(top, bottom)
+        bottom = result[0]
+        top = result[1]
     
     elif (instruction == "V"):
-        new_bottom = [bottom[1], bottom[0]]
-        new_top = [top[1], top[0]]
-
-        top = new_top
-        bottom = new_bottom
+        result = flip_vertical(top, bottom)
+        bottom = result[0]
+        top = result[1]
 
 print(" ".join(top))
 print(" ".join(bottom))
