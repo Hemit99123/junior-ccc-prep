d312f71d00f204ef2a9f52f22e9cc51504f17215
Hemit Patel <hemitvpatel@gmail.com>
2025-05-31 21:24:13 -0400

    Create donut shop.py

---
+++ 2025/donut shop.py
@@ -0,0 +1,13 @@
+total_donuts = int(input())
+total_events = int(input())
+
+for _ in range(total_events):
+  operation = input()
+  donuts = int(input())
+
+  if (operation == "+"):
+    total_donuts += donuts 
+  else:
+    total_donuts -= donuts
+
+print(total_donuts)
