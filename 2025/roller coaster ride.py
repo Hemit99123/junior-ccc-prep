0899809596f45521c826cec76bb2f80914d2e218
Hemit Patel <hemitvpatel@gmail.com>
2025-05-31 21:19:04 -0400

    Update roller coaster ride.py

--- 2025/roller coaster ride.py
+++ 2025/roller coaster ride.py
@@ -1,10 +1,16 @@
+# Hemit Patel
+# 781159
+
 place_in_line = int(input(""))
 number_of_cars = int(input(""))
 people_in_car = int(input(""))
 
 total_people_ride = number_of_cars * people_in_car
 
-if (total_people_ride <= place_in_line):
+# If the place in line is less than total people in ride, there is space for you to enter the ride
+if (place_in_line <= total_people_ride):
   print("yes")
+  
+# There is only one another circumstance
 else:
   print("no")
